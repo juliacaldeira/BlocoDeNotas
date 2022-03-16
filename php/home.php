c5808e5ff53c2d18e1c22978070b4bdaeeca584b
JuliaCaldeira <julia.pcfranca@gmail.com>
2022-03-15 21:48:16 -0300

    notas ok

--- php/home.php
+++ php/home.php
@@ -4,30 +4,26 @@ require("./aut.php");
 
     $user = user_atual();
     $conexao = mysqli_connect("localhost", "root", "", "bloco_notas");
-    $pegarNotas = mysqli_query($conexao, "SELECT nota_id FROM nota WHERE usuario_id = '$user'");
+    $pegarNotas = mysqli_query($conexao, "SELECT text FROM nota WHERE usuario_id = '$user'");
 
     $linhas = mysqli_num_rows($pegarNotas);
-    $dados = array('sla');
+    $dados = array();
     if($linhas > 0) {
-        $dados = mysqli_fetch_array($pegarNotas);
-        var_dump($dados);
-
+        while($coluna = mysqli_fetch_array($pegarNotas)) {
+            array_push($dados, $coluna['text']);
+        }
     }
+    
 ?>
     <html>
-    <link rel="stylesheet" href="">
-            <form method="POST" class="form">
-            <select name="ordem" id="ordem">
-                <option value="">Ordernar por</option>
-            </select>
-            </form>
-        </div>
-    
+    <link rel="stylesheet" href="../Css/home.css">
+        <div id="linha"></div>
         <div id="grid">
-            <?php foreach($dados as $arr) {
-                var_dump($dados);
-                echo '<div class="grid-item">'.$arr.'</div>';
-            } ?>
+           <?php 
+                foreach($dados as $arr) {
+                    echo '<div class="nota-grid">'.$arr.'</div>';
+                }
+            ?> 
         </div>
     
     </body>
